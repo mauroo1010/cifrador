7750dddad48c7f8139dd528f018d96617cd2a1df
mauroo1010 <maurogutierreztormo@gmail.com>
2024-05-28 11:58:45 +0200

    1

--- Cifrador.java
+++ Cifrador.java
@@ -1,7 +1,7 @@
 public class Cifrador
 {
-    
- public String cifrarTexto(String texto, int offset) {
+
+    public String cifrarTexto(String texto, int offset) {
         String resultado = "";
 
         int contador = 0;
@@ -24,6 +24,16 @@ public class Cifrador
         }
 
         return resultado;
-    }    
-    
+    }
+    public void mostrarResultadoDeCifrado(int offset) {
+        for (int i = 65; i <= 90; i++) { 
+            int resultado = (i - 65 + offset) % 26 + 65; 
+            System.out.println(i + " (" + (char) i + ") - 65 ('A' - valor base) + " + offset + " (offset) % 26 + 65 ('A') = " + resultado + " ('" + (char) resultado + "')");
+        }
+
+        for (int i = 97; i <= 122; i++) {
+            int resultado = (i - 97 + offset) % 26 + 97; 
+            System.out.println(i + " (" + (char) i + ") - 97 ('a' - valor base) + " + offset + " (offset) % 26 + 97 ('a') = " + resultado + " ('" + (char) resultado + "')");
+        }
+    }
 }
